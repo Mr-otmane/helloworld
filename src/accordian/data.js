6aabc70c31ca5b81cdbec44bf447fac6a564b93c
ghareb <otmaneanna2020@gmail.com>
2025-03-01 23:52:09 +0000

    add file styles.css and add the text in index.jsx

--- src/accordian/data.js
+++ src/accordian/data.js
@@ -1,31 +1,26 @@
 const data = [
-    {
-        id : '1',
-        question: "what are accordion components",
-        answer:
-        "Accordion componrnts are user interface elements used for organ"
-
-    },
-    {
-        id : '2',
-        question: "what are they used for",
-        answer:
-        "Accordion componrnts are user interface elements used for organ"
-
-    },
-    {
-        id : '3',
-        question: "Accordion as a musical instrument",
-        answer:
-        "Accordion componrnts are user interface elements used for organizing and presenting"
-
-    },
-    {
-        id : '4',
-        question: "Can I create an accordion component with a different framework?",
-        answer:
-        "Accordion componrnts are user interface elements used for organ"
-
-    },
+  {
+    id: "1",
+    question: "what are accordion components",
+    answer:
+      "Accordion componrnts are user interface elements used for organizing and presenting content in a collapsible manner. they typically consist of a header, content, and an expand/collapse action",
+  },
+  {
+    id: "2",
+    question: "what are they used for",
+    answer:
+      "They are commonly employed in various contexts, including FAQs, product descriptions, navigation menus, settings panels, and data tables, to save screen space and provide a stuctured and user-friendly interface for presenting information or options.",
+  },
+  {
+    id: "3",
+    question: "Accordion as a musical instrument",
+    answer:
+      "Accordion componrnts are user interface elements used for organizing and presenting",
+  },
+  {
+    id: "4",
+    question: "Can I create an accordion component with a different framework?",
+    answer: "Accordion componrnts are user interface elements used for organ",
+  },
 ];
 export default data;
